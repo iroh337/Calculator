777bbb559748e050673f7d754f169f9589a88c36
Richard Henrique <richardhs826@gmail.com>
2024-02-11 17:44:27 -0300

    Calculator

---
+++ index.js
@@ -0,0 +1,35 @@
+var display = document.querySelector('.display')
+
+
+function appendDisplay(input){
+    display.value += input
+}
+
+function dClear(){
+    display.value = ''
+}
+ 
+function calculate(){
+    try{
+        display.value = eval(display.value)
+        
+    }catch(error){
+        display.value = 'Error'
+        appendDisplay = null
+        
+        setTimeout(function(){
+            location.reload()
+            
+        },1400)
+        
+    }
+
+    if(display.value == 'undefined'){
+         appendDisplay = null
+        setTimeout(function(){
+            location.reload()
+        },1400)
+    
+    }
+}
+
